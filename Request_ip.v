29578d4517d74886c73d3aebefade3ee5402cdc1
khalyomede <khalyomede@gmail.com>
2025-11-24 22:31:41 +0100

    add metod to get ip from request

--- Request_ip.v
+++ Request_ip.v
@@ -6,15 +6,5 @@ import khalyomede.ip { Address, Ipv4, Ipv6 }
 pub fn (request Request) ip() !Address {
     remote_address := request.base_request.header.get_custom('RemoteAddr', HeaderQueryConfig{exact: true})!
 
-    is_ip_v6 := Ipv6.parse(remote_address) or { Ipv6{} } != Ipv6{}
-
-    if is_ip_v6 {
-        address := Ipv6.parse(remote_address)!
-
-        return Address(address)
-    }
-
-    address := Ipv4.parse(remote_address)!
-
-    return Address(address)
+    return Address.parse(remote_address)!
 }
--- Request_ip_test.v
+++ Request_ip_test.v
@@ -19,9 +19,32 @@ fn test_it_can_get_ip_v4_from_request() {
 
     request := web.Request.from(base_request)
     request_ip := request.ip()!
+
     actual := match request_ip {
         Ipv4 { request_ip.str() }
-        Ipv6 { request_ip.str() }
+        Ipv6 { '' }
+    }
+
+    expect(actual.str()).to_be_equal_to(expected)
+}
+
+fn test_it_can_get_ip_v6_from_request() {
+    mut fake := Faker{}
+    mut header := Header{}
+    expected := fake.ip_v6()
+
+    header.set_custom('RemoteAddr', expected)!
+
+    base_request := Request{
+        header: header
+    }
+
+    request := web.Request.from(base_request)
+    request_ip := request.ip()!
+
+    actual := match request_ip {
+        Ipv4 { '' }
+        Ipv6 { request_ip.to_full_string() }
     }
 
     expect(actual.str()).to_be_equal_to(expected)
